bbefdf0d05c662937a74abdea8c951755d3d6f22
pramod chouhdary <pramodchouhdary.71@gmail.com>
2023-09-10 13:39:33 +0530

    add new coloumn and userguide table1

---
+++ app/Models/Userguide.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Userguide extends Model
+{
+ 	protected $table = 'userguide';
+
+    use HasFactory;
+}
